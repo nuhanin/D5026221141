a32d4e392595140dc53fdb4bf6c53b42c1a7dc1a
Hanin Nuha Hafizhah <hfz.haninnuha@gmail.com>
2023-11-20 15:51:16 +0700

    Week13 Pertemuan 20 Nov 2023

--- routes/web.php
+++ routes/web.php
@@ -79,5 +79,13 @@ Route::get('/blog', 'App\Http\Controllers\BlogController@home');
 Route::get('/blog/tentang', 'App\Http\Controllers\BlogController@tentang');
 Route::get('/blog/kontak', 'App\Http\Controllers\BlogController@kontak');
 
+// route pegawai
+Route::get('/pegawai','App\Http\Controllers\PegawaiController@index1');
+Route::get('/pegawai/tambah','App\Http\Controllers\PegawaiController@tambah');
+Route::post('/pegawai/store','App\Http\Controllers\PegawaiController@store');
+Route::get('/pegawai/edit/{id}','App\Http\Controllers\PegawaiController@edit');
+Route::post('/pegawai/update','App\Http\Controllers\PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','App\Http\Controllers\PegawaiController@hapus');
+
 
 
