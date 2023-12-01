4b73db35987a3c270f41b05c3151b96e86c83c80
Hanin Nuha Hafizhah <hfz.haninnuha@gmail.com>
2023-12-01 21:12:09 +0700

    Tugas pertemuan 27 Nov 2023

--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -17,10 +17,24 @@
                <input name="nama" type = "text" class = "form-control" id = "nama" placeholder = "Masukkan nama pegawai">
             </div>
          </div>
-		Nama <input type="text" name="nama" required="required"> <br/>
-		Jabatan <input type="text" name="jabatan" required="required"> <br/>
-		Umur <input type="number" name="umur" required="required"> <br/>
-		Alamat <textarea name="alamat" required="required"></textarea> <br/>
-		<input type="submit" value="Simpan Data">
+		 <div class = "form-group">
+            <label for = "jabatan" class = "col-sm-2 control-label">Jabatan</label>
+            <div class = "col-sm-8">
+               <input name="jabatan" type = "text" class = "form-control" id = "jabatan" placeholder = "Masukkan jabatan pegawai">
+            </div>
+         </div>
+		 <div class = "form-group">
+            <label for = "umur" class = "col-sm-2 control-label">Umur</label>
+            <div class = "col-sm-8">
+               <input name="umur" type = "number" class = "form-control" id = "umur" placeholder = "Masukkan umur pegawai">
+            </div>
+         </div>
+		 <div class = "form-group">
+            <label for = "alamat" class = "col-sm-2 control-label">Alamat</label>
+            <div class = "col-sm-8">
+               <textarea name="alamat" class = "form-control" id = "alamat" placeholder = "Masukkan alamat pegawai"></textarea>
+            </div>
+         </div>
+		<input type="submit" value="Simpan Data" class="btn btn-primary">
 	</form>
 @endsection
---
+++ resources/views/view.blade.php
@@ -0,0 +1,6 @@
+@extends('master2')
+
+@section('konten')
+	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
+	<h3>Informasi Pegawai</h3>
+@endsection
